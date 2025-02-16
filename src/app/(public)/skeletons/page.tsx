d426472cb79ebe3433d4051d380e937c5d0d01ff
Mohmmed Aariz <mohmmed.ariz786@gmail.com>
2025-02-16 20:53:28 +0530

    edited deploy.yml

--- src/app/(public)/skeletons/page.tsx
+++ src/app/(public)/skeletons/page.tsx
@@ -2,15 +2,15 @@ import { NEXT_AUTH } from "@/app/api/auth/[...nextauth]/options";
 import { LatestContentSkeleton } from "@/app/ui/agency/dashboard/skeletons";
 import { SubscribersTableSkeleton } from "@/app/ui/agency/subscribers/skeletons";
 import { CardSkeleton, DisplayContentSkeleton, LeftLandingPageSkeleton, TitleSkeleton } from "@/app/ui/skeletons";
-import { getServerSession } from "next-auth";
+// import { getServerSession } from "next-auth";
 import { getToken } from "next-auth/jwt";
-
+// 
 export default async function Page(){
-    const session = getServerSession(NEXT_AUTH);
-    console.log(session);
+    // const session = getServerSession(NEXT_AUTH);
+    // console.log(session);
     return (
         <div className="flex flex-col">
-            <pre>{JSON.stringify(session)}</pre>
+            {/* <pre>{JSON.stringify(session)}</pre> */}
             {/* <CardSkeleton /> */}
             {/* <TitleSkeleton />
             <DisplayContentSkeleton />
