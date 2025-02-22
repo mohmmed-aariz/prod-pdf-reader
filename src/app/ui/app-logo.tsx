1fdf6c15580882f6d1bcbe95fa4f878c2d3532bb
Mohmmed Aariz <mohmmed.ariz786@gmail.com>
2025-02-22 14:36:20 +0530

    addded favicon

--- src/app/ui/app-logo.tsx
+++ src/app/ui/app-logo.tsx
@@ -8,15 +8,17 @@ export default function AppLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-gray-100 font-bold`}
 
     >
-      <GlobeAltIcon className="h-8 w-8 rotate-[15deg]" />
-      {/* <svg className="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" strokeLinejoin="round" strokeWidth="2" strokeLinecap="round" strokeMiterlimit="10" stroke="currentColor" fill="none">
+      {/* <GlobeAltIcon className="h-8 w-8 rotate-[15deg]" /> */}
+      <svg className="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" strokeLinejoin="round" strokeWidth="2" strokeLinecap="round" strokeMiterlimit="10" stroke="currentColor" fill="none">
           <rect x="3" y="1" width="7" height="12"></rect>
           <rect x="3" y="17" width="7" height="6"></rect>
           <rect x="14" y="1" width="7" height="6"></rect>
           <rect x="14" y="11" width="7" height="12"></rect>
-        </svg> */}
+        </svg>
 
-      <p className="text-[26px] ">Logo</p>
+      {/* <p className="text-[26px] ">Logo</p> */}
+      {/* <p className="text-lg ">Home</p> */}
+
     </div>
   );
 }
